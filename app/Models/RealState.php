07570407a4139a6f021ac3dd21d6b9a1096ada60
Lucas <lubapisilva@gmail.com>
2021-08-11 14:59:45 -0300

    fillable RealState Models

---
+++ app/Models/RealState.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RealState extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'title', 'description', 'content',
+        'price', 'slug', 'bedrooms', 'bathrooms', 'property_area', 'total_property_area'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
